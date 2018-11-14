eaf094702cc7201ec1aa657a0bbd99ef067ee317
Rebecca Miles <remiles@wpi.edu>
2018-11-13 19:29:58 -0500

    Completed setup

--- WezenSchedule/src/main/java/com/schedule/wezen/demo/LambdaFunctionHandler.java
+++ WezenSchedule/src/main/java/com/schedule/wezen/demo/LambdaFunctionHandler.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
 public class LambdaFunctionHandler implements RequestStreamHandler {
