e98fb36b4f5b88339d39a14fc1aa5acdfe45989c
avyannaconevargas <avyannaconevarga@wpi.edu>
2018-11-26 17:55:31 -0500

    New Imports

--- WezenSchedule/src/main/java/com/schedule/wezen/demo/LambdaFunctionHandler.java
+++ WezenSchedule/src/main/java/com/schedule/wezen/demo/LambdaFunctionHandler.java
@@ -1,11 +1,28 @@
 package com.schedule.wezen.demo;
 
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.amazonaws.SdkClientException;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.google.gson.Gson;
+
 
 public class LambdaFunctionHandler implements RequestStreamHandler {
 
